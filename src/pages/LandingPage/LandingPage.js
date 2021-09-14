3c103840e7ebec2c2f106c25bc5aafc85f51f9fd
A0007 <ajaybendre@credenceanalytics.com>
2021-09-15 03:08:53 +0530

    fixes for landing screen

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -3,7 +3,38 @@ import React from "react";
 const LandingPage = () => {
   return (
     <React.Fragment>
-      <div> landing page</div>
+      <header>
+        <div class="w-full bg-cover bg-center" style={{ height: '35rem', backgroundImage: 'url(/static/images/landing-screen.jpg)' }}>
+          <div class="flex items-center justify-start h-full w-full bg-gray-900 bg-opacity-50 px-24">
+            <div class="text-left">
+              <h1 class="text-yellow-300 text-2xl font-semibold uppercase md:text-4xl">Quick Reliable, Hamesha</h1>
+              <h1 class="text-white text-2xl mt-2 font-semibold md:text-lg w-96">Your ultimate travel companion. Carries all the information you need while travelling</h1>
+
+              <div class="get-app flex space-x-5 mt-10 justify-center md:justify-start">
+                <button class="apple bg-red-600 text-white shadow-xl px-7 py-2 rounded-lg flex items-center space-x-2">
+                  <div class="text">
+                    <p class="text-sm font-bold text-gray-900">Track Your <br /> Shipment</p>
+                  </div>
+                </button>
+                <button class="google bg-white shadow-xl px-7 py-2 rounded-lg flex items-center space-x-4">
+                  <div class="text">
+                    <p class="text-sm font-bold text-gray-900">Book Now</p>
+                  </div>
+                </button>
+              </div>
+              <div class="bg-white shadow p-2 flex rounded-md mt-10">
+                <span class="w-auto flex justify-end items-center text-gray-500 p-2">
+                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                </span>
+                <input class="w-full rounded p-2" type="text" placeholder="Try 'Tracking ID'" />
+                <button class="bg-red-600 hover:bg-red-300 rounded text-white p-2 pl-4 pr-4">
+                  <p class="font-semibold text-xs">Search</p>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
     </React.Fragment>
   );
 };
