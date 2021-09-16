0f7ead9ab70f5754f4f0e396a14d4d04d53d238a
A0007 <ajaybendre@credenceanalytics.com>
2021-09-16 12:39:08 +0530

    Fixes for Landing Page

---
+++ src/pages/LandingPage/components/AnnuallyDelivery/AnnuallyDelivery.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const AnnuallyDelivery = () => {
+    return (
+        <React.Fragment>
+            <section class="text-gray-600 body-font">
+                <div class="container mx-auto flex px-5 md:flex-row flex-col items-start">
+                    <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                        <div class="flex flex-wrap w-full mb-20">
+                            <div class="w-full mb-6 lg:mb-0">
+                                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Annually Delivering</h1>
+                                <div class="h-1 w-20 bg-indigo-500 rounded"></div>
+                            </div>
+                        </div>
+                        <h1 class="title-font sm:text-2xl text-3xl mb-4 font-medium text-gray-900">Annually Delivering over 80,000 MT to the Right Place at the Right Time at their Optimal Temperatures
+                        </h1>
+                    </div>
+                    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                        <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x400" />
+                    </div>
+                </div>
+            </section>
+        </React.Fragment>
+    )
+}
+
+export default AnnuallyDelivery;
---
+++ src/pages/LandingPage/components/index.js
@@ -0,0 +1,9 @@
+export { default as YearsOfExp } from "./YearsOfExp";
+export { default as AnnuallyDelivery } from "./AnnuallyDelivery";
+export { default as Solutions } from "./Solutions";
+export { default as Statistics } from "./Statistics";
+export { default as Testimonials } from "./Testimonials";
+
+
+
+
