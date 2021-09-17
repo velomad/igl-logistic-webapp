98a43f0522d71825258be8b55f715768d6aa4931
A0007 <ajaybendre@credenceanalytics.com>
2021-09-18 01:55:52 +0530

    Squashed commit of the following:
    commit cb9a0ba7ab03f85b83d44a0f91416ccdbf1eed98
    Author: A0007 <ajaybendre@credenceanalytics.com>
    Date:   Sat Sep 18 01:43:12 2021 +0530
    
        Fixes for landing screen
    
    commit 0f7ead9ab70f5754f4f0e396a14d4d04d53d238a
    Author: A0007 <ajaybendre@credenceanalytics.com>
    Date:   Thu Sep 16 12:39:08 2021 +0530
    
        Fixes for Landing Page
    
    commit 3c103840e7ebec2c2f106c25bc5aafc85f51f9fd
    Author: A0007 <ajaybendre@credenceanalytics.com>
    Date:   Wed Sep 15 03:08:53 2021 +0530
    
        fixes for landing screen

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,9 +1,56 @@
 import React from "react";
-
+import { YearsOfExp, AnnuallyDelivery, Solutions, Statistics, Testimonials } from './components';
 const LandingPage = () => {
   return (
     <React.Fragment>
-      <div> landing page</div>
+      <div className='space-y-12 sm:space-y-24'>
+        <section>
+          <div class="w-full bg-cover bg-center" style={{ height: '35rem', backgroundImage: 'url(/static/images/landing-screen.jpg)' }}>
+            <div class="flex items-center justify-start h-full w-full bg-gray-900 bg-opacity-50 sm:px-24">
+              <div class="text-center sm:text-left">
+                <h1 class="text-yellow-300 text-2xl font-semibold uppercase md:text-4xl">Quick Reliable, Hamesha</h1>
+                <h1 class="text-white text-xl mt-2 font-semibold md:text-lg sm:w-96">Your ultimate travel companion. Carries all the information you need while travelling</h1>
+                <div class="get-app flex space-x-5 mt-10 justify-center md:justify-start">
+                  <button class="apple bg-red-600 text-white shadow-xl px-7 py-2 rounded-lg flex items-center space-x-2">
+                    <div class="text">
+                      <p class="text-sm font-bold text-gray-900">Track Your <br /> Shipment</p>
+                    </div>
+                  </button>
+                  <button class="google bg-white shadow-xl px-7 py-2 rounded-lg flex items-center space-x-4">
+                    <div class="text">
+                      <p class="text-sm font-bold text-gray-900">Book Now</p>
+                    </div>
+                  </button>
+                </div>
+                <div class="bg-white shadow p-2 flex rounded-md mt-10 mx-10 sm:mx-0">
+                  <span class="w-auto flex justify-end items-center text-gray-500 p-2">
+                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                  </span>
+                  <input class="w-full rounded p-2" type="text" placeholder="Try 'Tracking ID'" />
+                  <button class="bg-red-600 hover:bg-red-300 rounded text-white px-2 py-2 sm:px-4 sm:py-2 ">
+                    <p class="font-semibold text-xs">Search</p>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <YearsOfExp />
+        </section>
+        <section>
+          <AnnuallyDelivery />
+        </section>
+        <section>
+          <Solutions />
+        </section>
+        <section>
+          <Statistics />
+        </section>
+        <section>
+          <Testimonials />
+        </section>
+      </div>
     </React.Fragment>
   );
 };
